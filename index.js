57fbcb23104db29df1317ecabfebeb001b7fc42f
Vatsal-1501 <vatsal@on2cook.com>
2026-01-13 15:57:42 +0530

    to download files

--- index.js
+++ index.js
@@ -35,6 +35,28 @@ app.post('/upload', (req, res) => {
     }
 });
 
+// Route to list and download files
+app.get('/files', (req, res) => {
+    fs.readdir(uploadDir, (err, files) => {
+        if (err) return res.status(500).send("Cannot read directory");
+        
+        // Generate simple HTML links for each file
+        let html = "<h1>Uploaded Logs</h1><ul>";
+        files.forEach(file => {
+            html += `<li><a href="/download/${file}">${file}</a></li>`;
+        });
+        html += "</ul>";
+        res.send(html);
+    });
+});
+
+// Route to handle the actual download
+app.get('/download/:name', (req, res) => {
+    const fileName = req.params.name;
+    const filePath = path.join(uploadDir, fileName);
+    res.download(filePath);
+});
+
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server active on port ${PORT}`);
 });
